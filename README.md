803f00c4f2a14a5c9848013f41b828a6ca8964c0
ticoneva <ticoneva@gmail.com>
2022-08-17 22:11:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jupyter-rsession-proxy-multi
+
+Add multiple RStudio configurations to Jupyter. Relies on a [modified version](https://github.com/scrp-cuhk/jupyter-rsession-proxy) of [`jupyter-rsession-proxy`](https://github.com/jupyterhub/jupyter-rsession-proxy).
---
+++ setup.py
@@ -0,0 +1,25 @@
+# Each entry in entry_points['jupyter_serverproxy_servers'] corresponds 
+# to one RStudio configuratiom in addition to the one loaded by 
+# jupyter-rsession-proxy.
+
+import setuptools
+
+setuptools.setup(
+    name="jupyter-rsession-proxy-multi",
+    version='1.0.0',
+    url="https://github.com/scrp-cuhk/jupyter-rsession-proxy-multi",
+    author="Vinci Chow",
+    description="Add additional RStudio proxies to Jupyter",
+    packages=setuptools.find_packages(),
+	keywords=['Jupyter'],
+	classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'jupyter-rsession-proxy'
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'rstudio_mkl = jupyter_rsession_proxy_multi:setup_rserver_mkl',
+            'rstudio_mro_4 = jupyter_rsession_proxy_multi:setup_rserver_mro'
+        ]
+    }
+)
