6a7b4c86b2c998441fa2103c49f07d15eb7f9793
ticoneva <ticoneva@gmail.com>
2022-08-18 22:51:11 +0800

    Change argument order

--- jupyter_rsession_proxy_multi/__init__.py
+++ jupyter_rsession_proxy_multi/__init__.py
@@ -2,14 +2,14 @@ from jupyter_rsession_proxy import setup_rserver
 
 # One function per RStudio configuration. 
 # Remember to add them as setup.py entry points.
-# Format: Display name, entry name, location of rserver configuration file
+# Format: entry name, display title, location of rserver configuration file
 def setup_rserver_mkl():
-    return setup_rserver("RStudio (R 4.1 MKL)",
-                         "rstudio_mkl_2022",
+    return setup_rserver("rstudio_mkl_2022",
+                         "RStudio (R 4.1 MKL)",
                          "/opt/network/R/rserver/rserver_mkl.conf")
 
 def setup_rserver_mro():
-    return setup_rserver("RStudio (R 4.0 MRO)",
-                         "rstudio_mro_4",
+    return setup_rserver("rstudio_mro_4",
+                         "RStudio (R 4.0 MRO)",
                          "/opt/network/R/rserver/rserver_mro.conf")
 
