1ddde2382983019c83c6fd9772d88531bcb2d8be
ticoneva <ticoneva@gmail.com>
2023-06-09 19:46:10 +0800

    Added Conda R

--- jupyter_rsession_proxy_multi/__init__.py
+++ jupyter_rsession_proxy_multi/__init__.py
@@ -3,9 +3,9 @@ from jupyter_rsession_proxy import setup_rserver
 # One function per RStudio configuration. 
 # Remember to add them as setup.py entry points.
 # Format: entry name, display title, location of rserver configuration file
-def setup_rserver_mkl():
-    return setup_rserver("rstudio_mkl_2022",
-                         "RStudio (R 4.1 MKL)",
+def setup_rserver_conda():
+    return setup_rserver("rstudio_conda",
+                         "RStudio (Conda)",
                          "/opt/network/R/rserver/rserver_mkl.conf")
 
 def setup_rserver_mro():
